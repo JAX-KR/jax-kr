dfee48886fb49d471f4e40447e54daf36dc1d7e5
mezcalagave <68863637+mezcalagave@users.noreply.github.com>
2023-03-05 22:08:43 +0900

    initial commit

---
+++ conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'jax-kr'
+copyright = '2023, jax-kr'
+author = 'jax-kr'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ko'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
---
+++ tutorial-docs/index.rst
@@ -0,0 +1,21 @@
+JAX-KR 튜토리얼
+++++++++++++++++
+
+해당 페이지는 `JAX 101`_
+에 있는 내용을 JAX/Flax LAB에서 번역한 문서입니다.
+
+.. _JAX 101: https://jax.readthedocs.io/en/latest/jax-101/index.html
+
+JAX 튜토리얼을 직접 작업한 사람들은 DeepMind에 소속되어 있는 엔지니어와 리서쳐가 만들었습니다.
+
+목차
+  * ㅁㅁ
+  * ㅁㅁ
+  * ㅁㅁ
+  * ㅁㅁ
+  * ㅁㅁ
+  * ㅁㅁ
+  * ㅁㅁ
+  
+    * ㄴㄴ 
+   
